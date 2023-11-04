32b7aea8ea498840551379894ed5e873f432eec0
Bistaaa <michele.bistarelli.mb@outlook.it>
2023-11-04 16:55:54 +0100

    aggiunta API video youtube e stile. manca sicurezza richieste

--- src/components/MainContent-components/BestTrades.vue
+++ src/components/MainContent-components/BestTrades.vue
@@ -2,8 +2,30 @@
 export default {
     name: 'BestTrades',
     data() {
-
+        return {
+            images: [
+                '4.png',
+                '10.png',
+                '20.png',
+                '4.png',
+                '10.png',
+                '20.png',
+                '4.png',
+                '10.png',
+                '20.png',
+                '4.png',
+                '10.png',
+                '20.png',
+                '4.png',
+                '10.png',
+                '20.png',
+                '4.png',
+                '10.png',
+                '20.png',
+            ]
+        };
     },
+
     methods: {
 
     },
@@ -12,19 +34,21 @@ export default {
 
 <template>
     <div id="best-trades-container">
-        <span class="category-icon">
+        <span class="category-icon-black">
             <font-awesome-icon icon="fa-solid fa-chart-line" />
         </span>
         <h2>I MIGLIORI TRADE DELL'ANNO</h2>
         <div id="trades-container">
-
+            <div class="trade-image-container" v-for="(image, index) in images" :key="index">
+                <img :src="'src/assets/img/' + image" class="trades-images" alt="Trade Image" />
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
 #best-trades-container {
-    background-color: #ffffff;
+    background-color: #ECC01B;
     padding: 100px 20%;
     width: 100%;
     display: flex;
@@ -36,9 +60,22 @@ export default {
         font-size: 65px;
         text-align: center;
         text-decoration: underline;
-        margin-bottom: 30px;
+        margin-bottom: 70px;
     }
 
-    #trades-container {}
+    #trades-container {
+        display: flex;
+        flex-wrap: wrap;
+        width: 100%;
+
+        .trade-image-container {
+            width: calc(100% / 4);
+            padding: 10px;
+
+            .trades-images {
+                width: 100%;
+            }
+        }
+    }
 }
 </style>
--- src/components/MainContent-components/MyVideos.vue
+++ src/components/MainContent-components/MyVideos.vue
@@ -2,8 +2,24 @@
 export default {
     name: 'MyVideos',
     data() {
-
+        return {
+            videos: []
+        }
     },
+
+    mounted() {
+
+        //API YouTube
+        fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCb1ZKE1eay_VPglsP0Xp7EQ&maxResults=12&order=date&key=AIzaSyB-CVWdt8T-gAE5nyTxSKy32w0UfrbgSe0")
+            .then((data) => {
+                return data.json();
+            })
+            .then((data) => {
+                console.log(data.items);
+                this.videos = data.items;
+            })
+    },
+
     methods: {
 
     },
@@ -15,9 +31,17 @@ export default {
         <span class="category-icon">
             <font-awesome-icon icon="fa-solid fa-video" />
         </span>
-        <h2>I MIEI VIDEO</h2>
+        <h2>I MIEI ULTIMI VIDEO</h2>
         <div id="videos-container">
-
+            <div class="single-video-container" v-for="video in videos" :key="video.id">
+                <a :href="'https://www.youtube.com/watch?v=' + video.id.videoId" target="_blank">
+                    <div class="video-thumbnail-container">
+                        <img :src="video.snippet.thumbnails.high.url" class="video-thumbnail" alt="">
+                        <img src="../../assets/img/overlay-video/Overlay-50.png" class="video-thumbnail-overlay" alt="">
+                    </div>
+                </a>
+                <h3 class="video-title-container">{{ video.snippet.title }}</h3>
+            </div>
         </div>
     </div>
 </template>
@@ -25,7 +49,7 @@ export default {
 <style scoped lang="scss">
 #my-videos-container {
     background-color: #ffffff;
-    padding: 100px 20%;
+    padding: 100px 10%;
     width: 100%;
     display: flex;
     flex-direction: column;
@@ -39,6 +63,52 @@ export default {
         margin-bottom: 30px;
     }
 
-    #videos-container {}
+    #videos-container {
+        display: flex;
+        flex-wrap: wrap;
+
+        .single-video-container {
+            width: calc(100% / 4);
+            padding: 30px 20px;
+
+            .video-thumbnail-container {
+                width: 100%;
+                padding-top: calc(9 / 16 * 100%);
+                overflow: hidden;
+                position: relative;
+
+                .video-thumbnail {
+                    background-size: cover;
+                    position: absolute;
+                    top: 50%;
+                    left: 50%;
+                    transform: translate(-50%, -50%);
+                    width: 100%;
+                    z-index: 5;
+                }
+
+                .video-thumbnail-overlay {
+                    background-size: cover;
+                    position: absolute;
+                    top: 50%;
+                    left: 50%;
+                    transform: translate(-50%, -50%);
+                    width: 105%;
+                    z-index: 6;
+                    opacity: 0;
+                    transition: opacity 0.3s;
+                    cursor: pointer;
+
+                    &:hover {
+                        opacity: 1;
+                    }
+                }
+            }
+
+            .video-title-container {
+                margin-top: 10px;
+            }
+        }
+    }
 }
 </style>
