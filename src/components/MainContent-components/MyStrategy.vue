dd3ea114ecac49444a19a5e8c54b53b03eee3aa1
Bistaaa <michele.bistarelli.mb@outlook.it>
2023-11-14 23:41:59 +0100

    mediaquery mystrategy

--- src/components/MainContent-components/MyStrategy.vue
+++ src/components/MainContent-components/MyStrategy.vue
@@ -23,16 +23,18 @@ export default {
             <p class="category-icon">
                 <font-awesome-icon icon="fa-solid fa-bullseye" />
             </p>
-            <h2>LA NOSTRA STRATEGIA VINCENTE</h2>
-            <h3>Perchè scegliere Active Forex Trading?</h3>
+            <h2 id="mystrategy-title">LA NOSTRA STRATEGIA VINCENTE</h2>
+            <h3 id="second-mystrategy-title">Perchè scegliere Active Forex Trading?</h3>
             <div id="strategy-description-container">
                 <div class="neon-border" id="left-description-container">
                     <span></span>
                     <span></span>
                     <span></span>
                     <span></span>
-                    <h3>STOPPARE LE PERDITE SUL NASCERE E LASCIAR CORRERE I PROFITTI</h3>
-                    <p>La nostra strategia si basa sul motto "STOPPARE LE PERDITE SUL NASCERE E LASCIAR CORRERE I PROFITTI".
+                    <h3 class="mystrategy-description-title">STOPPARE LE PERDITE SUL NASCERE E LASCIAR CORRERE I PROFITTI
+                    </h3>
+                    <p class="mystrategy-description-text">La nostra strategia si basa sul motto "STOPPARE LE PERDITE SUL
+                        NASCERE E LASCIAR CORRERE I PROFITTI".
                         Impostiamo trade con stop-loss stretti in punti chiave del grafico e take profit medio-lunghi.
                         Questo ci permette di bloccare le perdite e andare in positivo con la prima operazione che parte
                         nella direzione giusta.
@@ -43,8 +45,9 @@ export default {
                     <span></span>
                     <span></span>
                     <span></span>
-                    <h3>UNICA STRATEGIA ADATTA ANCHE A CONTI MEDIO PICCOLI</h3>
-                    <p>La caratteristica distintiva della nostra strategia è il suo adattamento anche ai conti
+                    <h3 class="mystrategy-description-title">UNICA STRATEGIA ADATTA ANCHE A CONTI MEDIO PICCOLI</h3>
+                    <p class="mystrategy-description-text">La caratteristica distintiva della nostra strategia è il suo
+                        adattamento anche ai conti
                         medio-piccoli,
                         grazie agli stop loss stretti che permettono draw-down minimi evitando perdite significative, al
                         contrario di molti trader che non usano stop-loss e ti fanno bruciare il conto.
@@ -70,7 +73,7 @@ export default {
         padding: 100px 10%;
         background-color: rgba($color: #ffffff, $alpha: 0.7);
 
-        h2 {
+        #mystrategy-title {
             text-align: center;
             font-size: 60px;
             margin-bottom: 10px;
@@ -78,11 +81,27 @@ export default {
             text-decoration: underline;
         }
 
-        h3 {
+        @media screen and (max-width: 601px) {
+
+            #mystrategy-title {
+                font-size: 50px;
+                margin-bottom: 70px;
+            }
+        }
+
+        #second-mystrategy-title {
             text-align: center;
             font-size: 40px;
             margin-bottom: 150px;
             color: #000000;
+        }
+
+        @media screen and (max-width: 601px) {
+
+            #second-mystrategy-title {
+                font-size: 25px;
+                margin-bottom: 100px;
+            }
         }
 
         #strategy-description-container {
@@ -102,15 +121,29 @@ export default {
                     transform: scale(1.1);
                 }
 
-                h3 {
+                .mystrategy-description-title {
                     font-size: 35px;
                     text-align: center;
                     margin-bottom: 30px;
                 }
 
-                p {
+                .mystrategy-description-text {
                     font-size: 23px;
                     text-align: justify;
+                }
+
+                @media screen and (max-width: 601px) {
+
+                    .mystrategy-description-title {
+                        font-size: 25px;
+                    }
+                }
+
+                @media screen and (max-width: 601px) {
+
+                    .mystrategy-description-text {
+                        font-size: 20px;
+                    }
                 }
             }
 
@@ -125,16 +158,54 @@ export default {
                     transform: scale(1.1);
                 }
 
-                h3 {
+                .mystrategy-description-title {
                     font-size: 35px;
                     text-align: center;
                     margin-bottom: 30px;
                 }
 
-                p {
+                .mystrategy-description-text {
                     font-size: 23px;
                     text-align: justify;
                 }
+
+                @media screen and (max-width: 601px) {
+
+                    .mystrategy-description-title {
+                        font-size: 25px;
+                    }
+                }
+
+                @media screen and (max-width: 601px) {
+
+                    .mystrategy-description-text {
+                        font-size: 20px;
+                    }
+                }
+            }
+
+            @media screen and (max-width: 901px) {
+
+                #left-description-container,
+                #right-description-container {
+                    width: 80%;
+                }
+            }
+
+            @media screen and (max-width: 601px) {
+
+                #left-description-container,
+                #right-description-container {
+                    width: 100%;
+                }
+            }
+        }
+
+        @media screen and (max-width: 901px) {
+            #strategy-description-container {
+                align-items: center;
+                flex-direction: column;
+                gap: 50px;
             }
         }
     }
