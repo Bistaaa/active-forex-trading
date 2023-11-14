4cdb8d804b97b3bff80acb3ee02a8cd5fba40fef
Bistaaa <michele.bistarelli.mb@outlook.it>
2023-11-14 17:56:56 +0100

    media query jumbotron

--- src/components/MainContent-components/Jumbotron.vue
+++ src/components/MainContent-components/Jumbotron.vue
@@ -20,9 +20,9 @@ export default {
 <template>
   <div id="jumbotron-container">
     <div id="jumbotron-content-container">
-      <h1>Il nostro motto:
+      <h1 id="jumbotron-title">Il nostro motto:
         <br>
-        <span>"Stoppare le perdite sul nascere e lasciar correre i profitti"</span>
+        <span id="jumbotron-span">"Stoppare le perdite sul nascere e lasciar correre i profitti"</span>
       </h1>
       <div id="left-column">
         <h3>Servizi gratuiti sbloccando il gruppo privato Elite:</h3>
@@ -106,7 +106,7 @@ export default {
 
 <style scoped lang="scss">
 #jumbotron-container {
-  background-image: url(../../assets/img/10.png);
+  background-image: url(../../assets/img/jumbotron-background.png);
   background-size: cover;
   width: 100%;
 
@@ -116,16 +116,39 @@ export default {
     background-color: rgba($color: #ffffff, $alpha: 0.7);
     padding: 100px 5%;
 
-    h1 {
+    #jumbotron-title {
       padding: 0 15%;
       font-size: 61px;
       text-align: center;
 
-      span {
+      #jumbotron-span {
         font-size: 70px;
         text-align: center;
         color: #ECC01B;
         -webkit-text-stroke: 2px black;
+      }
+    }
+
+    @media screen and (max-width: 901px) {
+
+      #jumbotron-title {
+        padding: 0 10%;
+        font-size: 55px;
+
+        #jumbotron-span {
+          font-size: 65px;
+        }
+      }
+    }
+
+    @media screen and (max-width: 601px) {
+      #jumbotron-title {
+        padding: 0;
+        font-size: 35px;
+
+        #jumbotron-span {
+          font-size: 40px;
+        }
       }
     }
 
@@ -137,6 +160,12 @@ export default {
         font-size: 35px;
         margin-bottom: 30px;
         text-decoration: underline;
+      }
+
+      @media screen and (max-width: 601px) {
+        h3 {
+          font-size: 30px;
+        }
       }
 
       ul {
@@ -152,9 +181,39 @@ export default {
             font-weight: bold;
             line-height: 35px;
           }
+
+          @media screen and (max-width: 601px) {
+            li {
+              font-size: 15px;
+              line-height: normal;
+              text-align: left;
+            }
+          }
+        }
+
+        @media screen and (max-width: 601px) {
+          .bonus-container {
+            margin-bottom: 10px;
+          }
         }
       }
     }
+
+    @media screen and (max-width: 901px) {
+
+      #left-column {
+        width: 100%;
+        margin: 100px 0;
+        text-align: center;
+      }
+    }
+  }
+}
+
+@media screen and (max-width: 901px) {
+
+  #jumbotron-container {
+    background-position: 50% 40%;
   }
 }
 </style>
