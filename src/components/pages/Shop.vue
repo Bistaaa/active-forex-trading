0e429c39b9e5cdf315287e43bc22791510d54c3f
Bistaaa <michele.bistarelli.mb@outlook.it>
2023-11-25 16:31:35 +0100

    creata struttura shop, stile da fare

--- src/components/pages/Shop.vue
+++ src/components/pages/Shop.vue
@@ -3,30 +3,64 @@ export default {
     name: 'Shop',
     data() {
         return {
-            selectedItemID: null,
+            selectedItemID: 1,
+            selectedItemObject: {},
             items: [
-                { id: 1, name: 'Corso', price: 300 },
-                { id: 2, name: '1 to 1', price: 100 },
-                { id: 3, name: 'Indicatore', price: 500 },
+                {
+                    id: 1,
+                    name: 'Corso',
+                    description: `questa è la descrizione del corso: Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ea earum consequatur omnis laudantium eum nam hic animi necessitatibus voluptates, repudiandae corporis quasi nihil odit nulla qui, aut molestiae voluptatum!
+                    Ducimus quasi minus, deserunt sequi sit, odit autem illo non dolorem assumenda quas culpa repudiandae reprehenderit eos itaque voluptate eaque minima, rem ex unde doloremque? Illum nemo necessitatibus dignissimos at?`,
+                    price: 300
+                },
+                {
+                    id: 2,
+                    name: '1 to 1',
+                    description: `questa è la descrizione del 1 to 1: Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ea earum consequatur omnis laudantium eum nam hic animi necessitatibus voluptates, repudiandae corporis quasi nihil odit nulla qui, aut molestiae voluptatum!
+                    Ducimus quasi minus, deserunt sequi sit, odit autem illo non dolorem assumenda quas culpa repudiandae reprehenderit eos itaque voluptate eaque minima, rem ex unde doloremque? Illum nemo necessitatibus dignissimos at?`,
+                    price: 100
+                },
+                {
+                    id: 3,
+                    name: 'Indicatore',
+                    description: `questa è la descrizione dell'indicatore: Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ea earum consequatur omnis laudantium eum nam hic animi necessitatibus voluptates, repudiandae corporis quasi nihil odit nulla qui, aut molestiae voluptatum!
+                    Ducimus quasi minus, deserunt sequi sit, odit autem illo non dolorem assumenda quas culpa repudiandae reprehenderit eos itaque voluptate eaque minima, rem ex unde doloremque? Illum nemo necessitatibus dignissimos at?`,
+                    price: 500
+                },
+                {
+                    id: 4,
+                    name: 'Abbonamento Mensile',
+                    description: `questa è la descrizione dell'abbonamento: Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ea earum consequatur omnis laudantium eum nam hic animi necessitatibus voluptates, repudiandae corporis quasi nihil odit nulla qui, aut molestiae voluptatum!
+                    Ducimus quasi minus, deserunt sequi sit, odit autem illo non dolorem assumenda quas culpa repudiandae reprehenderit eos itaque voluptate eaque minima, rem ex unde doloremque? Illum nemo necessitatibus dignissimos at?`,
+                    price: 80
+                },
             ]
         };
     },
+
+    mounted() {
+        this.handleSelection()
+    },
+
     methods: {
         handleSelection() {
+            const paypalButtonsContainer = document.getElementById('paypal-buttons');
+            paypalButtonsContainer.innerHTML = '';
+
             console.log('Elemento selezionato:', this.selectedItemID);
-            const selectedItemObject = this.items.find(item => item.id == this.selectedItemID)
-            console.log(selectedItemObject.price);
+            this.selectedItemObject = this.items.find(item => item.id == this.selectedItemID);
+            console.log(this.selectedItemObject.price);
 
             window.paypal.Buttons({
                 createOrder(data, actions) {
-                    console.log(data, actions)
+                    console.log(data, actions);
                     return actions.order.create({
                         purchase_units: [{
                             amount: {
-                                value: selectedItemObject.price
+                                value: this.selectedItemObject.price
                             }
                         }]
-                    })
+                    });
                 },
                 onApprove(data, actions) {
                     return actions.order.capture().then(function (details) {
@@ -36,7 +70,7 @@ export default {
                 onError(error) {
                     console.log(error);
                 },
-            }).render("#paypal-buttons")
+            }).render("#paypal-buttons");
         },
     }
 }
@@ -48,15 +82,52 @@ export default {
 
 <template>
     <div id="shop-overall-container">
-        <label for="items">Seleziona un frutto:</label>
-        <select id="items" v-model="selectedItemID" @change="handleSelection">
-            <option v-for="item in items" :key="item.id" :value="item.id">{{ item.name }}</option>
-        </select>
+        <h2 id="shop-title">NEGOZIO</h2>
+        <div id="items-choice">
+            <label for="items">Seleziona un servizio:</label>
+            <select id="items" v-model="selectedItemID" @change="handleSelection">
+                <option v-for="item in items" :key="item.id" :value="item.id">{{ item.name }}</option>
+            </select>
+        </div>
+        <div class="item-container">
+            <h3 class="item-name">{{ selectedItemObject.name }}</h3>
+            <p class="item-description">{{ selectedItemObject.description }}</p>
+            <p class="item-price">Prezzo: {{ selectedItemObject.price }}€</p>
+        </div>
+        <div id="paypal-buttons"></div>
     </div>
-    <div id="paypal-buttons"></div>
 </template>
 
   
 
   
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+#shop-overall-container {
+    padding: 100px 10%;
+
+    #shop-title {
+        font-size: 55px;
+        text-align: center;
+    }
+
+    #items-choice {
+        font-size: 30px;
+    }
+
+    .item-container {
+        width: 100%;
+
+        .item-name {
+            font-size: 40px;
+        }
+
+        .item-description {
+            font-size: 25px;
+        }
+
+        .item-price {
+            font-size: 35px;
+        }
+    }
+}
+</style>
