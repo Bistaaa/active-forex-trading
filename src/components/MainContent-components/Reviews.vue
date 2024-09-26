8964523c6dd60f844117d0baf425bcbd9007e312
Bistaaa <michele.bistarelli.mb@outlook.it>
2024-09-26 16:10:34 +0200

    aggiunto stop scorrimento immagini, aggiustate dimenzioni varie e aggiunto ingrandimento review.

--- src/components/MainContent-components/Reviews.vue
+++ src/components/MainContent-components/Reviews.vue
@@ -13,28 +13,59 @@ export default {
                 '/img/trustpilot-reviews/review-7.png',
                 '/img/trustpilot-reviews/review-8.png',
             ],
-            currentIndex: 0
+            currentIndex: 0,
+            intervalId: null, // Aggiungi una proprietà per l'ID dell'intervallo
+            isFullscreen: false,
         }
     },
+
     methods: {
+
+        startInterval() {
+            this.intervalId = setInterval(this.nextImage, 5000); // Inizializza l'intervallo
+        },
+        stopInterval() {
+            clearInterval(this.intervalId); // Ferma l'intervallo
+        },
+
         nextImage() {
+            this.stopInterval(); // Ferma l'intervallo prima di cambiare immagine
             this.currentIndex = (this.currentIndex + 1) % this.images.length;
+            this.startInterval(); // Riavvia l'intervallo
         },
         prevImage() {
+            this.stopInterval(); // Ferma l'intervallo prima di cambiare immagine
             this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+            this.startInterval(); // Riavvia l'intervallo
         },
+
         getPrevIndex() {
             return (this.currentIndex - 1 + this.images.length) % this.images.length;
         },
         getNextIndex() {
             return (this.currentIndex + 1) % this.images.length;
+        },
+
+        openFullscreen() {
+            this.stopInterval(); // Ferma l'intervallo quando si apre il fullscreen
+            this.isFullscreen = true; // Attiva la modalità a schermo intero
+        },
+        closeFullscreen() {
+            this.isFullscreen = false; // Disattiva la modalità a schermo intero
+            this.startInterval(); // Riavvia l'intervallo quando si chiude il fullscreen
         }
     },
+
     mounted() {
-        setInterval(this.nextImage, 5000);
+        this.startInterval(); // Avvia l'intervallo al montaggio del componente
+    },
+
+    beforeDestroy() {
+        this.stopInterval(); // Ferma l'intervallo quando il componente viene distrutto
     }
 }
 </script>
+
 
 <template>
     <div id="reviews-container">
@@ -50,7 +81,7 @@ export default {
                         <img :src="images[getPrevIndex()]" alt="Previous review image">
                     </div>
                     <!-- Immagine centrale (attuale) -->
-                    <div class="carousel-item active-image">
+                    <div class="carousel-item active-image" @click="openFullscreen">
                         <img :src="images[currentIndex]" alt="Current review image">
                     </div>
                     <!-- Immagine destra (successiva) -->
@@ -61,6 +92,10 @@ export default {
                 <button @click="prevImage" class="carousel-control prev">‹</button>
                 <button @click="nextImage" class="carousel-control next">›</button>
             </div>
+        </div>
+
+        <div v-if="isFullscreen" class="fullscreen-modal" @click="closeFullscreen">
+            <img :src="images[currentIndex]" class="fullscreen-image" alt="Fullscreen review image">
         </div>
     </div>
 </template>
@@ -78,12 +113,12 @@ export default {
         font-size: 65px;
         text-align: center;
         text-decoration: underline;
-        margin-bottom: 60px;
+        margin-bottom: 70px;
     }
 
     #trustpilot-img-container {
         height: 60px;
-        margin-bottom: 80px;
+        margin-bottom: 70px;
 
         #trustpilot-img {
             height: 100%;
@@ -138,6 +173,7 @@ export default {
                 width: 40%;
                 opacity: 1;
                 transform: scale(1);
+                cursor: pointer;
             }
 
             .carousel-control {
@@ -161,6 +197,25 @@ export default {
                 }
             }
         }
+    }
+
+    /* Stili per la modal a schermo intero */
+    .fullscreen-modal {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(128, 128, 128, 0.8);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        z-index: 1000;
+    }
+
+    .fullscreen-image {
+        width: 50%;
+        height: auto;
     }
 }
 </style>
